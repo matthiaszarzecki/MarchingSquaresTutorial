f248336031c18e3420578cc0f773ce9faecccb88
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2021-04-03 14:02:25 +0200

    Add cell cases

--- MarchingSquaresUnity/Assets/Scripts/VoxelGrid.cs
+++ MarchingSquaresUnity/Assets/Scripts/VoxelGrid.cs
@@ -19,7 +19,7 @@ public class VoxelGrid : MonoBehaviour {
   public void Initialize(int resolution, float size) {
     this.resolution = resolution;
     voxelSize = size / resolution;
-    voxels = new bool[resolution * resolution];
+    voxels = new Voxel[resolution * resolution];
 
     voxelMaterials = new Material[voxels.Length];
 
@@ -113,6 +113,101 @@ public class VoxelGrid : MonoBehaviour {
     if (d.state) {
       cellType |= 8;
     }
+
+    switch (cellType) {
+      case 0:
+        return;
+      case 1:
+        AddTriangle(a.position, a.yEdgePosition, a.xEdgePosition);
+        break;
+      case 2:
+        AddTriangle(b.position, a.xEdgePosition, b.yEdgePosition);
+        break;
+      case 4:
+        AddTriangle(c.position, c.xEdgePosition, a.yEdgePosition);
+        break;
+      case 8:
+        AddTriangle(d.position, b.yEdgePosition, c.xEdgePosition);
+        break;
+      case 3:
+        AddQuad(a.position, a.yEdgePosition, b.yEdgePosition, b.position);
+        break;
+      case 5:
+        AddQuad(a.position, c.position, c.xEdgePosition, a.xEdgePosition);
+        break;
+      case 10:
+        AddQuad(a.xEdgePosition, c.xEdgePosition, d.position, b.position);
+        break;
+      case 12:
+        AddQuad(a.yEdgePosition, c.position, d.position, b.yEdgePosition);
+        break;
+      case 15:
+        AddQuad(a.position, c.position, d.position, b.position);
+        break;
+
+      case 7:
+        AddPentagon(a.position, c.position, c.xEdgePosition, b.yEdgePosition, b.position);
+        break;
+      case 11:
+        AddPentagon(b.position, a.position, a.yEdgePosition, c.xEdgePosition, d.position);
+        break;
+      case 13:
+        AddPentagon(c.position, d.position, b.yEdgePosition, a.xEdgePosition, a.position);
+        break;
+      case 14:
+        AddPentagon(d.position, b.position, a.xEdgePosition, a.yEdgePosition, c.position);
+        break;
+      case 6:
+        AddTriangle(b.position, a.xEdgePosition, b.yEdgePosition);
+        AddTriangle(c.position, c.xEdgePosition, a.yEdgePosition);
+        break;
+      case 9:
+        AddTriangle(a.position, a.yEdgePosition, a.xEdgePosition);
+        AddTriangle(d.position, b.yEdgePosition, c.xEdgePosition);
+        break;
+    }
+  }
+
+  private void AddTriangle(Vector3 a, Vector3 b, Vector3 c) {
+    int vertexIndex = vertices.Count;
+    vertices.Add(a);
+    vertices.Add(b);
+    vertices.Add(c);
+    triangles.Add(vertexIndex);
+    triangles.Add(vertexIndex + 1);
+    triangles.Add(vertexIndex + 2);
+  }
+
+  private void AddQuad(Vector3 a, Vector3 b, Vector3 c, Vector3 d) {
+    int vertexIndex = vertices.Count;
+    vertices.Add(a);
+    vertices.Add(b);
+    vertices.Add(c);
+    vertices.Add(d);
+    triangles.Add(vertexIndex);
+    triangles.Add(vertexIndex + 1);
+    triangles.Add(vertexIndex + 2);
+    triangles.Add(vertexIndex);
+    triangles.Add(vertexIndex + 2);
+    triangles.Add(vertexIndex + 3);
+  }
+
+  private void AddPentagon(Vector3 a, Vector3 b, Vector3 c, Vector3 d, Vector3 e) {
+    int vertexIndex = vertices.Count;
+    vertices.Add(a);
+    vertices.Add(b);
+    vertices.Add(c);
+    vertices.Add(d);
+    vertices.Add(e);
+    triangles.Add(vertexIndex);
+    triangles.Add(vertexIndex + 1);
+    triangles.Add(vertexIndex + 2);
+    triangles.Add(vertexIndex);
+    triangles.Add(vertexIndex + 2);
+    triangles.Add(vertexIndex + 3);
+    triangles.Add(vertexIndex);
+    triangles.Add(vertexIndex + 3);
+    triangles.Add(vertexIndex + 4);
   }
 
   private void CreateVoxel(int i, int x, int y) {
